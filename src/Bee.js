b21a20d33a044f61990181fd73f33278b36b0bdb
MachTech <aheri@Macbook-Pro.local>
2015-10-14 14:51:43 -0700

    Completed Bee class functionality and passed Bee tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype); //this delegates the Grub.prototype as default lookkup location
+Bee.prototype.constructor = Bee;
+
+// var myCutieBee = new Bee();
+// console.log('myCutieBee is: ', myCutieBee);
--- src/Grub.js
+++ src/Grub.js
@@ -3,8 +3,9 @@ var Grub = function() {
 this.age = 0;
 this.color = 'pink';
 this.food = 'jelly';
-Grub.prototype.eat = function(){}; //This is important - remember, it's not this.eat but Name.prototype.method so it can be the next levels prototype.
 
 };
+
+Grub.prototype.eat = function(){}; //This is important - remember, it's not this.eat but Name.prototype.method so it can be the next levels prototype.
 
 //var myGrubbie = new Grub();
